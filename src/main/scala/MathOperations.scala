cbebf139ea951d8034da42deb409148998dd98e9
Vinay Kumar <vinay.kumar@knoldus.in>
2018-01-09 16:36:35 +0530

    Issue of Companion object resolved. It works

--- src/main/scala/MathOperations.scala
+++ src/main/scala/MathOperations.scala
@@ -61,3 +61,39 @@ class MathOperations {
   }
 }
 
+object MathOperations {
+  val conduit: MathOperations = new MathOperations
+
+  def main(args: Array[String]): Unit = {
+    printList()
+    printMaximum()
+    printFibonacciAt()
+    printSumAndProduct()
+  }
+
+  def printList(): Unit = {
+    val aList: List[Int] = List(10, 20, 30, 40)
+    conduit.displayListWithIndex(aList)
+  }
+
+  def printMaximum(): Unit = {
+    val aList: List[Int] = List(1, 5, 2, 9, 7)
+    val max: Int = conduit.findMaxFromList(aList)
+    println(s"Maximum element is : $max")
+  }
+
+  def printFibonacciAt(): Unit = {
+    val position: Int = 4
+    val term: Int = conduit.findFibonacciAt(position)
+    println(s"Fibonacci Term at $position : $term")
+  }
+
+  def printSumAndProduct(): Unit = {
+    val number: Int = 5
+    val product: Long = conduit.factorial(number)
+    val sum: Long = conduit.findSumOfDigits(product)
+    println(s"Product from 1 to $number : $product")
+    println(s"Sum of each digit of $product: $sum")
+  }
+}
+
